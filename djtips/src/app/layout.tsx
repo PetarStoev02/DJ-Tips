3292ccb035bc9b9d6e3b128150008c837d23d457
Петър Стоев <petarstoev0505@gmail.com>
2024-03-14 12:30:28 +0200

    feat: Landing Page Layout

--- djtips/src/app/layout.tsx
+++ djtips/src/app/layout.tsx
@@ -17,12 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full">
-      <body className={cn("relative h-full font-sans antialiased", inter.className)}>
-        <main className='relative flex flex-col min-h-screen'>
-      
-              {children}
-  
-        </main></body>
+      <body
+        className={cn("relative h-full font-sans antialiased", inter.className)}
+      >
+        <main className="relative flex flex-col min-h-screen">
+          <div flex-grow flex-1>{children}</div>
+        </main>
+      </body>
     </html>
   );
 }
